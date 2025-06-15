863d8dd0c6e1e166277dd491d558b43c562bbf08
Moeer03 <161110786+Moeer03@users.noreply.github.com>
2025-06-15 18:35:03 +0500

    Update streamlit app.py

--- streamlit app.py
+++ streamlit app.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from data_preprocessing import preprocess_data
+from sklearn.preprocessing import StandardScaler
 # Load data once
 data = pd.read_csv("WA_Fn-UseC_-HR-Employee-Attrition.csv")
 def main():
